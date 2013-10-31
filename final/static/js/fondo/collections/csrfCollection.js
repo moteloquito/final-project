e8ef41e197daab24de29beac28b3d9a20f590901
Marcos <marcoslewis@gmail.com>
2013-10-31 17:11:33 -0300

    Cookie jquery plugin

--- final/static/js/fondo/collections/csrfCollection.js
+++ final/static/js/fondo/collections/csrfCollection.js
@@ -1,16 +1,16 @@
 define([
   'jquery',
+  'jquerycookie',
   'backbone'
-], function($, B) {
+], function($, Cookie, B) {
 
   var CSRFCollection = B.Collection.extend({
 
     fetch: function(params) {
       var options = params || {};
-      var csrftoken = this.getCookie('csrftoken');
 
       options.beforeSend = function(xhr) {
-        xhr.setRequestHeader('X-CSRFToken', csrftoken);
+        xhr.setRequestHeader('X-CSRFToken', $.cookie('csrftoken'));
       };
 
       options.type = 'POST';
@@ -18,20 +18,6 @@ define([
       options.reset = true;
       
       return B.Collection.prototype.fetch.call(this, options);
-    },
-
-    getCookie: function(name) {
-      var cookieValue = null;
-      if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = $.trim(cookies[i]);
-          if (cookie.substring(0, name.length + 1) == (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          }
-        }
-      }
-      return cookieValue;
     }
 
   });
--- final/static/js/fondo/main.js
+++ final/static/js/fondo/main.js
@@ -1,39 +1,43 @@
 requirejs.config({
-    baseUrl: '/static/js',
-    paths: {
-	backbone: '/static/js/libs/backbone/backbone',
-	bootstrap: '/static/js/libs/bootstrap/bootstrap',
-	jquery: '/static/js/libs/jquery/jquery',
-	tpl: '/static/js/libs/tpl/tpl',
-	underscore: '/static/js/libs/underscore/underscore',
-	app: '/static/js/fondo/app'
+  baseUrl: '/static/js',
+  paths: {
+    backbone: '/static/js/libs/backbone/backbone',
+    bootstrap: '/static/js/libs/bootstrap/bootstrap',
+    jquery: '/static/js/libs/jquery/jquery',
+    jquerycookie: '/static/js/libs/jquery/jquery.cookie',
+    tpl: '/static/js/libs/tpl/tpl',
+    underscore: '/static/js/libs/underscore/underscore',
+    app: '/static/js/fondo/app'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
     },
-    shim: {
-	backbone: {
-	    deps: ['underscore', 'jquery'],
-	    exports: 'Backbone'
-	},
-	underscore: {
-	    exports: '_'
-	},
-	bootstrap: ['jquery']
-    }
+    underscore: {
+      exports: '_'
+    },
+    jquerycookie: {
+      deps: ['jquery'],
+      exports: 'Cookie'
+    },
+    bootstrap: ['jquery']
+  }
 });
 
 requirejs([
-    'backbone',
-    'bootstrap',
-    'jquery',
-    'underscore',
-    'app',
-    'fondo/routers/fondo_router',
-    'fondo/views/mainView'
-],
-function(Backbone, Bootstrap, $, _, App, FondoRouter, MainView) {
-    var router = new FondoRouter();
-    var mainView = new MainView();
+  'backbone',
+  'bootstrap',
+  'jquery',
+  'underscore',
+  'app',
+  'fondo/routers/fondo_router',
+  'fondo/views/mainView'
+], function(Backbone, Bootstrap, $, _, App, FondoRouter, MainView) {
+  var router = new FondoRouter();
+  var mainView = new MainView();
 
-    Backbone.history.start();
+  Backbone.history.start();
 
-    router.render(mainView);
+  router.render(mainView);
 });
