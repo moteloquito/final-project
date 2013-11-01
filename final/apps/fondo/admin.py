861229097d8613869c368c3efcfcbfa69fb99c00
Marcos <marcoslewis@gmail.com>
2013-11-01 16:11:56 -0300

    Ticket to admin

--- final/apps/fondo/admin.py
+++ final/apps/fondo/admin.py
@@ -1,4 +1,5 @@
-from final.apps.fondo.models import Fondo
+from final.apps.fondo.models import Fondo, Ticket
 from django.contrib import admin
 
 admin.site.register(Fondo)
+admin.site.register(Ticket)
