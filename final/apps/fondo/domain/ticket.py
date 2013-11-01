1e3991af5cf08f842ee9a6b64f9071a848410647
Marcos <marcoslewis@gmail.com>
2013-11-01 16:12:07 -0300

    Ticket unicode

--- final/apps/fondo/domain/ticket.py
+++ final/apps/fondo/domain/ticket.py
@@ -7,6 +7,9 @@ class Ticket(Model):
     date = DateField()
     fondo = ForeignKey(Fondo)
 
+    def __unicode__(self):
+        return "%s - %s: %0.2f" % (self.fondo.name, self.description, self.value)
+
     class Meta:
         db_table = 'ticket'
         app_label = 'fondo'
